21b134a7156d3a08c601645033690ad7da150355
Iago Bozza <iagobozza@writeonlycode.io>
2022-12-20 04:04:16 +0000

    Fix SignedOut menu

--- app/javascript/components/Menu/MenuUserSignedOut.js
+++ app/javascript/components/Menu/MenuUserSignedOut.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { ActionIcon, Menu } from "@mantine/core";
-import { IconDotsVertical, IconKey, IconLogin, IconUserPlus } from "@tabler/icons";
+import {
+  IconDotsVertical,
+  IconKey,
+  IconLogin,
+  IconUserPlus,
+} from "@tabler/icons";
 
 import LoginModal from "../Users/LoginModal";
 import RegisterModal from "../Users/RegisterModal";
@@ -9,13 +14,13 @@ import PasswordsNewModal from "../Users/PasswordsNewModal";
 const MenuUserSignedOut = ({ loading }) => {
   const [loginModalOpened, setLoginModalOpened] = useState(false);
   const [registerModalOpened, setRegisterModalOpened] = useState(false);
-  const [resetPasswordModalOpened, setResetPasswordModalOpened] = useState(false);
+  const [resetPasswordModalOpened, setResetPasswordModalOpened] =
+    useState(false);
 
   return (
     <>
-      <Menu
-        position="left"
-        control={
+      <Menu position="left">
+        <Menu.Target>
           <ActionIcon
             loading={loading}
             loaderProps={{ size: "xs", variant: "dots" }}
@@ -25,26 +30,27 @@ const MenuUserSignedOut = ({ loading }) => {
           >
             <IconDotsVertical />
           </ActionIcon>
-        }
-      >
-        <Menu.Item
-          onClick={() => setLoginModalOpened(true)}
-          icon={<IconLogin size={14} />}
-        >
-          Login
-        </Menu.Item>
-        <Menu.Item
-          onClick={() => setRegisterModalOpened(true)}
-          icon={<IconUserPlus size={14} />}
-        >
-          Register
-        </Menu.Item>
-        <Menu.Item
-          onClick={() => setResetPasswordModalOpened(true)}
-          icon={<IconKey size={14} />}
-        >
-          Reset Password
-        </Menu.Item>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item
+            onClick={() => setLoginModalOpened(true)}
+            icon={<IconLogin size={14} />}
+          >
+            Login
+          </Menu.Item>
+          <Menu.Item
+            onClick={() => setRegisterModalOpened(true)}
+            icon={<IconUserPlus size={14} />}
+          >
+            Register
+          </Menu.Item>
+          <Menu.Item
+            onClick={() => setResetPasswordModalOpened(true)}
+            icon={<IconKey size={14} />}
+          >
+            Reset Password
+          </Menu.Item>
+        </Menu.Dropdown>
       </Menu>
       <LoginModal opened={loginModalOpened} setOpened={setLoginModalOpened} />
       <RegisterModal
